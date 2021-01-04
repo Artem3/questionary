188428a469cc1505fb644cb4c1680ade3b3c011c
Artem3 <artem.goncharenko@gmail.com>
2021-01-04 19:14:05 +0200

    'react-router-bootstrap' dependency added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
