e1a5ac5e4cb8c80884fbdb7250523a776ebd3974
Artem3 <artem.goncharenko@gmail.com>
2021-01-29 16:22:33 +0200

    Dummy table with list of saved questionaries

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
 
 export default function List() {
-  const [lists, setLists] = useState(localStorage);
-  
-  return (
-    <Container /* style={{ minHeight: "100vh", color: "white" }}*/>
-      <h3>List of you saved questionnaires</h3>
+  // const [lists, setLists] = useState(localStorage);
 
-      <ListGroup.Item disabled>Cras justo odio</ListGroup.Item>
-      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-    </Container>
+  return (
+    <>
+      <Container style={{ minHeight: "100vh", color: "white" }}>
+        <h4>The list of saved questionaries </h4>
+        <Table bordered hover size="sm" variant="light">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>List title</th>
+              <th>Question number</th>
+              <th>Run</th>
+              <th>Edit</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {for (let [key, value] of Object.entries(localStorage))  {
+          console.log(`${key}: ${value}`)
+        } }
+            {/* <tr key={i.id}> */}
+            <tr>
+              <td>{1}</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </>
   );
 }
