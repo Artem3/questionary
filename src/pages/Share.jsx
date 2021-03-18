6092dc3ac539de6b6c28a72215ffe453465b2baf
Artem3 <artem.goncharenko@gmail.com>
2021-03-18 10:29:54 +0200

    GetAll records from Firebase through REST

--- src/pages/Share.jsx
+++ src/pages/Share.jsx
@@ -1,31 +1,62 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import firebase from './../firebase';
+import { v4 as uuid } from 'uuid';
 
 function Share() {
-  const [lists, setLists] = useState([{ id: 'test', questions: ['a', 'b'] }]);
-  console.log('---Share1--');
+  const [lists, setLists] = useState([]);
 
-  const ref = firebase.firestore().collection('lists');
+  const axios = require('axios').default;
+  const ref = firebase.firestore().collection('userId-9');// ownerId
 
-  useEffect(() => {
-    console.log('---Share2--');
-    getAllLists();
-  }, []);
+  const getAllLists = () => {
+    const url =
+      'https://firestore.googleapis.com/v1/projects/questionnaire-4f52b/databases/(default)/documents/userId-9/';
+    axios
+      .get(url)
+      .then(function (response) {
+        console.log(response.data.documents);
+        response.data.documents.map((elem) => console.log(elem.name));
+        console.log('---------');
+        response.data.documents.map((elem) => console.log(elem.fields));
+        // response.data.documents.map((elem) =>
+        //   console.log(elem.fields.questions.arrayValue.values.map((val) => console.log(val.mapValue.fields)))
+        // );
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-  function getAllLists() {
-    ref.get().then((item) => {
-      const items = item.docs.map((doc) => doc.data());
-      console.log(items);
-      setLists(items);
-    });
-  }
+  const createList = () => {
+    const test = {
+      listId: 'listId-11',
+      listName: 'lisName-11',
+      questions: [
+        { question: 'q9', expectedAnswer: 'a9' },
+        { question: 'q10', expectedAnswer: 'a10' },
+      ],
+    };
+    const testString = JSON.stringify(test);
+    const toDbObj = { doc: testString };
+
+    ref
+      .doc('listId-' + uuid()) //listId
+      .set(toDbObj)
+      .catch((err) => {
+        console.error(err);
+      });
+    console.log('createList-after');
+  };
 
   return (
     <Container style={{ minHeight: '100vh', color: 'white' }}>
       <h3>Firebase Share test page </h3>
 
       <button onClick={getAllLists}>Get All questionnaires from DB</button>
+      <br />
+      <br />
+      <button onClick={createList}>Create new questionnaires</button>
 
       <div>
         <h3>All:</h3>
